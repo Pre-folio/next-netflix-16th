e1c65fddfe55393c320ce0a774e84aab320a1819
강나연 <seoultownroad@naver.com>
2022-11-09 19:24:54 +0900

    feat: styled-component 세팅

---
+++ components/Navigation.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Navigation = () => <StyledNavigation>This is nav</StyledNavigation>;
+
+export default Navigation;
+
+const StyledNavigation = styled.nav`
+  background: yellow;
+`;
