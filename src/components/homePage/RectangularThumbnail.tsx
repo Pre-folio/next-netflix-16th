dc8d608888d1b0cc2b60bce269ff3789225d6a0c
yjoonjang <yjoonjang34@gmail.com>
2022-11-10 02:48:50 +0900

    feat: upcoming api 연결 및 이미지 썸네일 컴포넌트 레이아웃 제작

---
+++ src/components/homePage/RectangularThumbnail.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const RectangularThumbnail = () => {
+  return <Wrapper>s</Wrapper>;
+};
+
+export default RectangularThumbnail;
+
+const Wrapper = styled.div`
+  width: 103px;
+  height: 161px;
+  background-color: black;
+`;
