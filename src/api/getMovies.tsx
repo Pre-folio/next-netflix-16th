f829be74bd969f99304698a7cf74d91f3b90704b
yjoonjang <yjoonjang34@gmail.com>
2022-11-10 02:35:33 +0900

    feat: homePage 관련 api 불러오기 완료

--- src/api/getMovies.tsx
+++ src/api/getMovies.tsx
@@ -4,3 +4,11 @@ const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
 export const getNowPlaying = () => {
   return client.get(`movie/now_playing?api_key=${API_KEY}`).then((res) => res.data);
 };
+
+export const getTopRated = () => {
+  return client.get(`movie/top_rated?api_key=${API_KEY}`).then((res) => res.data);
+};
+
+export const getPopular = () => {
+  return client.get(`movie/popular?api_key=${API_KEY}`).then((res) => res.data);
+};
