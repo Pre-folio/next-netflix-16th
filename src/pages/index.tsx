98eda8a9f8f8443084ed339a075a32379c54baac
강나연 <seoultownroad@naver.com>
2022-11-10 17:13:02 +0900

    feat: 로고 로티 애니메이션 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import Navigation from '../components/elements/Navigation';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import Image from "next/image";
+import Navigation from "../components/elements/Navigation";
+import LogoLottie from "../components/landingPage/LogoLottie";
+import styles from "../styles/Home.module.css";
 
 const LandingPage = () => {
-  return <div>landingPage</div>;
+  return (
+    <div>
+      <LogoLottie />
+    </div>
+  );
 };
 
 export default LandingPage;
