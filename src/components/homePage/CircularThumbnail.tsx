ac3b43b14416de5b4808bed8f355ea578919daa3
yjoonjang <yjoonjang34@gmail.com>
2022-11-10 17:43:42 +0900

    feat: homePage 이미지 썸네일에 링크 연결

--- src/components/homePage/CircularThumbnail.tsx
+++ src/components/homePage/CircularThumbnail.tsx
@@ -1,14 +1,27 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { IThumbnailProps } from '../../interfaces/interface';
+import Link from 'next/link';
+import Image from 'next/image';
 
-const CircularThumbnail = () => {
-  return <Wrapper>{/* <Image /> */}</Wrapper>;
+const CircularThumbnail = ({ onClick, imageSrc }: IThumbnailProps) => {
+  return (
+    <Wrapper onClick={onClick}>
+      <Link href={'/home/id'}>
+        <Image src="" width={102} height={102} alt="원형 이미지" />
+      </Link>
+    </Wrapper>
+  );
 };
 
 export default CircularThumbnail;
 
-const Wrapper = styled.div`
+const Wrapper = styled.button`
   width: 102px;
   height: 102px;
   border-radius: 50%;
   background-color: white;
+  overflow: hidden;
+  border: none;
+
+  cursor: pointer;
 `;
