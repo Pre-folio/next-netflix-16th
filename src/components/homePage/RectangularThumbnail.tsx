735a8288386a5ca60f59d4e926425d38a3af1730
yjoonjang <yjoonjang34@gmail.com>
2022-11-10 17:43:42 +0900

    feat: homePage 이미지 썸네일에 링크 연결

--- src/components/homePage/RectangularThumbnail.tsx
+++ src/components/homePage/RectangularThumbnail.tsx
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { IThumbnailProps } from '../../interfaces/interface';
 
 const RectangularThumbnail = () => {
   return <Wrapper>s</Wrapper>;
---
+++ src/interfaces/interface.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export interface IThumbnailProps {
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+  imageSrc: string;
+}
