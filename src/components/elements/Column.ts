8c70f1eae04b1f50e8b33e5f113e18df1a756515
yjoonjang <yjoonjang34@gmail.com>
2022-11-11 02:16:48 +0900

    fix: Column 컴포넌트 marginHeight 속성 추가

--- src/components/elements/Column.ts
+++ src/components/elements/Column.ts
@@ -2,6 +2,7 @@ import { IWrapperProps } from './../../interfaces/interface';
 import styled from 'styled-components';
 
 export const Column = styled.div.attrs<IWrapperProps>((props) => ({
+  marginTop: props.marginTop || 0,
   marginBottom: props.marginBottom || 0,
   justifyContent: props.justifyContent || 'center',
   alignItems: props.alignItems || 'flex-start',
@@ -11,6 +12,7 @@ export const Column = styled.div.attrs<IWrapperProps>((props) => ({
   flex-direction: column;
   justify-content: ${(props) => props.justifyContent};
   align-items: ${(props) => props.alignItems};
+  margin-top: ${(props) => props.marginTop};
   margin-bottom: ${(props) => props.marginBottom};
   gap: ${(props) => props.gap};
 `;
