36f34bdfb8c3b84cfc12c4630151c74915d6a626
yjoonjang <yjoonjang34@gmail.com>
2022-11-11 20:42:51 +0900

    fix: movieListBar 관련 타입 오류 수정

--- src/interfaces/interface.ts
+++ src/interfaces/interface.ts
@@ -1,8 +1,23 @@
-import React from 'react';
-
 export interface IThumbnailProps {
   imageSrc: string;
-  id: string;
+  id: number;
+}
+
+export interface IMovies {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: number;
+  vote_average: number;
+  vote_count: number;
 }
 
 export interface IWrapperProps {
