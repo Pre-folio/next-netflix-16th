73f2428c96aaec47c427c62a8a54a9ac2fc8860f
강나연 <seoultownroad@naver.com>
2022-11-11 21:01:00 +0900

    feat: footer 버튼 클릭 시 아이콘 색상 변경 기능 추가

--- src/components/icons/HomeIcon.tsx
+++ src/components/icons/HomeIcon.tsx
@@ -1,28 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { SvgWrapper } from "./icons.styled";
 
-interface HomeIconProps {
-  onClick?: React.MouseEventHandler<HTMLButtonElement>;
-  className?: string;
-  isSelected?: boolean;
-}
+const HomeIcon = ({ path, onClick, isSelected }: any) => {
+  // const [isSelected, setIsSelected] = useState(false);
+  // useEffect(() => {
+  //   setIsSelected(path === "home");
+  // }, [path]);
 
-const HomeIcon = ({ onClick, className, isSelected }: HomeIconProps) => {
   return (
-    <div className={className}>
+    <SvgWrapper name="home" onClick={onClick}>
       <svg
         width="24"
         height="24"
         viewBox="0 0 24 24"
-        fill={isSelected ? 'white' : 'black'}
+        fill={isSelected ? "white" : "#8C8787"}
         xmlns="http://www.w3.org/2000/svg"
       >
-        <path d="M22 9.15789L12 2" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-        <path d="M2 9.15789L12 2" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-        <path d="M5 19V7.36841" stroke="white" stroke-width="2" stroke-linecap="round" />
-        <path d="M19 19V7.36841" stroke="white" stroke-width="2" stroke-linecap="round" />
-        <path d="M5 19H19" stroke="white" stroke-width="2" stroke-linecap="round" />
+        <path
+          d="M22 9.15789L12 2"
+          stroke={isSelected ? "white" : "#8C8787"}
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M2 9.15789L12 2"
+          stroke={isSelected ? "white" : "#8C8787"}
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M5 19V7.36841"
+          stroke={isSelected ? "white" : "#8C8787"}
+          stroke-width="2"
+          stroke-linecap="round"
+        />
+        <path
+          d="M19 19V7.36841"
+          stroke={isSelected ? "white" : "#8C8787"}
+          stroke-width="2"
+          stroke-linecap="round"
+        />
+        <path
+          d="M5 19H19"
+          stroke={isSelected ? "white" : "#8C8787"}
+          stroke-width="2"
+          stroke-linecap="round"
+        />
       </svg>
-    </div>
+    </SvgWrapper>
   );
 };
 
--- src/states/footerState.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from 'recoil';
-
-export const isClickedState = atom({
-  key: 'footerState/isClicked',
-  default: [],
-});
