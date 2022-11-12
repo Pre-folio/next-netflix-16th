455c39a239afec9b2587a6d1781131b25b5b1345
yjoonjang <yjoonjang34@gmail.com>
2022-11-12 19:55:20 +0900

    style: font 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
-import '../styles/globals.css';
+import GlobalStyle from '../styles/GlobalStyle';
 import { RecoilRoot } from 'recoil';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
+      <GlobalStyle />
       <Component {...pageProps} />
     </RecoilRoot>
   );
