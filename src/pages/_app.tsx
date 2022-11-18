2d6578eee7cd575a3f44ad1d2da2194296c168ce
강나연 <seoultownroad@naver.com>
2022-11-18 20:58:43 +0900

    fix: Mixed content 해결

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
 import GlobalStyle from '../styles/GlobalStyle';
 import '../styles/fonts.css';
 import { RecoilRoot } from 'recoil';
-import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 import { useState } from 'react';
 import Head from 'next/head';
@@ -13,7 +17,11 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Netflix</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
+        <meta
+          http-equiv='Content-Security-Policy'
+          content='upgrade-insecure-requests'
+        />
       </Head>
       <QueryClientProvider client={queryClient}>
         <Hydrate state={pageProps.dehydratedState}>
