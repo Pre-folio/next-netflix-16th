f096d899ae6322ac86a3ebce246edf8e1cbbea7f
장영준 <jang-youngjoon@jang-yeongjun-ui-MacBookAir.local>
2022-11-18 14:12:00 +0900

    feat: react-query로 homePage 데이터 fetching

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,28 @@
 import GlobalStyle from '../styles/GlobalStyle';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import '../styles/fonts.css';
 import { RecoilRoot } from 'recoil';
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 import { useState } from 'react';
 import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
+
   return (
     <>
       <Head>
         <title>Netflix</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <RecoilRoot>
-        <GlobalStyle />
-        <QueryClientProvider client={queryClient}>
-          <Component {...pageProps} />
-        </QueryClientProvider>
-      </RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <RecoilRoot>
+            <GlobalStyle />
+            <Component {...pageProps} />
+          </RecoilRoot>
+        </Hydrate>
+      </QueryClientProvider>
     </>
   );
 }
