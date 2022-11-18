353dac426836cd3ed8db2c9256717e84ded0259c
강나연 <seoultownroad@naver.com>
2022-11-18 19:10:07 +0900

    feat: favicon 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,15 +4,22 @@ import '../styles/fonts.css';
 import { RecoilRoot } from 'recoil';
 import type { AppProps } from 'next/app';
 import { useState } from 'react';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
   return (
-    <RecoilRoot>
-      <GlobalStyle />
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
-    </RecoilRoot>
+    <>
+      <Head>
+        <title>Netflix</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <RecoilRoot>
+        <GlobalStyle />
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </RecoilRoot>
+    </>
   );
 }
