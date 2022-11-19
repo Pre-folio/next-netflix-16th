e90e6a3bd42b3299de2712d20544cbe316c02d3f
강나연 <seoultownroad@naver.com>
2022-11-19 23:10:18 +0900

    feat: 한 글자만 되는(...) 검색 + 무한스크롤

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 import { getPopular, searchMovies } from '../../api/getMovies';
 import Footer from '../../components/elements/Footer';
 import SearchBox from '../../components/searchPage/SearchBox';
-import SearchList from '../../components/searchPage/SearchList';
+
 import { useDebounce } from '../../hooks/useDebounce';
 import { selectedContentState } from '../../states/footerState';
 import { searchedMoviesState, searchWordState } from '../../states/searchState';
@@ -13,19 +13,22 @@ import { useInfiniteScrollQuery } from '../../components/searchPage/useInfiniteS
 import { useInView } from 'react-intersection-observer';
 import SkeletonItem from '../../components/searchPage/SkeletonItem';
 import SearchItem from '../../components/searchPage/SearchItem';
+import InitialList from '../../components/searchPage/InitialList';
+import SearchList from '../../components/searchPage/SearchList';
 
-const SearchPage = (searchedData: any) => {
+const SearchPage = () => {
   const router = useRouter();
-
   const [selectedIcon, setSelectedIcon] = useRecoilState(selectedContentState);
   const pageName = router.asPath.slice(1);
   const [searchWord, setSearchWord] = useRecoilState(searchWordState);
   const debouncedSearchWord = useDebounce(searchWord, 500);
-  const [searchedMovies, setSearchedMovies] = useRecoilState(searchedMoviesState);
+  const [searchedMovies, setSearchedMovies] =
+    useRecoilState(searchedMoviesState);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   // const [popularPageLength, setPopularPageLength] = useState<number>(1);
   const [searchedPageLength, setSearchedPageLength] = useState<number>(1);
-  const { getBoard, getNextPage, getBoardIsSuccess, getNextPageIsPossible } = useInfiniteScrollQuery();
+  const { getBoard, getNextPage, getBoardIsSuccess, getNextPageIsPossible } =
+    useInfiniteScrollQuery();
   const [ref, isView] = useInView();
 
   useEffect(() => {
@@ -60,36 +63,7 @@ const SearchPage = (searchedData: any) => {
   return (
     <SearchPageContainer>
       <SearchBox />
-      <div className="home-wrapper">
-        {debouncedSearchWord ? <></> : (
-        {
-          // 데이터를 불러오는데 성공하고 데이터가 0개가 아닐 때 렌더링
-          getBoardIsSuccess && getBoard!.pages
-            ? getBoard!.pages.map((page_data: any, page_num: any) => {
-                const board_page = page_data.board_page;
-                console.log(board_page);
-
-                return board_page?.map((item: any, idx: any) => {
-                  if (
-                    // 마지막 요소에 ref 달아주기
-                    getBoard!.pages.length - 1 === page_num &&
-                    board_page.length - 1 === idx
-                  ) {
-                    return (
-                      // 마지막 요소에 ref 넣기 위해 div로 감싸기
-                      <div ref={ref} key={item.board_id}>
-                        <SkeletonItem key={item.board_id} />
-                      </div>
-                    );
-                  } else {
-                    return <SearchItem key={item.board_id} {...item} />;
-                  }
-                });
-              })
-            : null
-        }
-        )}
-      </div>
+      {debouncedSearchWord ? <SearchList /> : <InitialList />}
       {/* <SearchList isLoading={isLoading} /> */}
       <Footer />
     </SearchPageContainer>
